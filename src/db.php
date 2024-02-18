6abbf7b83e9090c471c9050be7c2b27b48f6ea84
shiro-av <vadas25@yandex.ru>
2024-02-18 19:38:55 +0500

    пример отправки запроса к БД и вывод на страницу результата запроса в виде таблицы

--- src/db.php
+++ src/db.php
@@ -12,4 +12,23 @@ if (!$connect) {
     die('Could not connect: ' . mysql_error());
 }
 echo 'Connected successfully<br>';
-var_dump($connect);
+$query = 'SELECT * from users';
+$result = mysqli_query($connect, $query) or die('Invalid query: ' . mysql_error());
+
+$str = '<table border="1">';
+$str .= '<tr>';
+for($i=0;$i< mysqli_num_fields($result); $i++) {
+    $field_info = mysqli_fetch_field_direct($result, $i);
+    $str .= '<th>'. $field_info->name . '</th>';
+}
+$str .= '</tr>';
+while( $row = mysqli_fetch_assoc($result)) {
+       $str .= '<tr>';
+       foreach ($row as $el) {
+           $str .= '<td>'. $el. '</td>';
+    }
+        $str .= '</tr>';
+}
+$str .= '</table>';
+echo $str;
+mysqli_close($connect);
